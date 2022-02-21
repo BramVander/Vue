b397eb916c20a288832faa2266398001b4f3185e
Vander <bram1105@hotmail.com>
2022-02-21 13:37:13 +0100

    logo + welcome

--- real-estate-care/src/App.vue
+++ real-estate-care/src/App.vue
@@ -1,20 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Real Estate Care" name="Bram Vander" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
+:root {
+  /* colors unrecognized
+  --text: rgba(41, 52, 57, 0);
+  --cyan: rgba(0, 170, 162, 0);
+  --gradientBlue: rgba(71, 94, 108, 0);
+  --gradientDark: rgba(20, 27, 31, 0); 
+  */
+
+  /* store colors */
+  --text: #293439;
+  --cyan: #00aaa2;
+  --gradientLight: #475e6c;
+  --gradientDark: #141b1f;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- real-estate-care/src/components/HelloWorld.vue
+++ real-estate-care/src/components/HelloWorld.vue
@@ -1,38 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-
+    <p>Developed by {{ name }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+    name: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.hello {
+  background-color: #00aaa2;
 }
 </style>
